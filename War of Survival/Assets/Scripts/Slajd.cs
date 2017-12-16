465521ecc1f4f960e11d731ce5f54de097e348cd
kurs <kurs@systempro.local>
2017-12-16 15:25:43 +0100

    Proba

---
+++ War of Survival/Assets/Scripts/Slajd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Slajd : MonoBehaviour {
+
+    public Slider mainSlider;
+
+	// Use this for initialization
+	void Start () {
+        mainSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ValueChangeCheck()
+    {
+        Debug.Log(mainSlider.value);
+    }
+}
